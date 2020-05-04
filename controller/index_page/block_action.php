befac445d17bb310d8dc6e4ef70b11a651e4462a
axe <axebnoo@gmail.com>
2020-05-04 19:51:48 +0800

    index_table change2

--- controller/index_page/block_action.php
+++ controller/index_page/block_action.php
@@ -5,22 +5,11 @@ function func2() {
 //    echo 'Hello from 2';
 }
 
-echo $_GET['user_num'];
+$v = $_GET['user_num'];
 
+$output = shell_exec('(echo "LGI:OP=\"vlrdata\",PWD =\"!QAZ2wsx\";";sleep 5; echo "USE ME:MEID=5;";sleep 5; echo "LST CNACLD: RSNAME=\"NEW_KT\", QR=LOCAL;";sleep 5) | telnet 10.132.0.160 6000');
+echo "<pre>$output</pre>";
 
-//switch($_GET['user_num']) {
-//    case '1':
-//        func1();
-//        break;
-//    case '2':
-//        func2();
-//        break;
-//    case '3':
-//        func3();
-//        break;
-//    default:
-        // Do nothing?
-//}
 ?>
 
 
