1d422a1852af341ceb485aa8ffa7c3c121bb3dbc
axe <axebnoo@gmail.com>
2020-05-04 19:26:20 +0800

    index_table change2

--- controller/index_page/block_action.php
+++ controller/index_page/block_action.php
@@ -1,6 +1,12 @@
 <?php
 function func1() {echo 'Hello from 1';}
-function func2() {echo 'Hello from 2';}
+function func2() {
+    $v = 123;
+    $any = '(echo "LGI:OP=\"tuguldur\",PWD =\"Azure_0309\";";sleep 5; echo "USE ME:MEID=5;";sleep 5; echo "ADD CALLPRICHK: CSCNAME=\"UNITEL\", PFX=K\'00825761080898, CPFX=K\'EEEEEEEE, PCDN=\"INVALID\", PT=INHIBITED, FCC=CV45;"; sleep 5) | telnet 10.132.0.160 6000 > /home/core/log/'.$v.'_$(date +%Y-%m-%d_%H:%M).log';
+    $output = shell_exec($any);
+    echo "<pre>$output </pre>";
+//    echo 'Hello from 2';
+}
 function func3() {echo 'Hello from 3';}
 switch($_GET['func']) {
     case '1':
