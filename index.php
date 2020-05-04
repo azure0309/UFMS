2d7d3a117384e820cc842cef29df4768db52d5a9
axe <axebnoo@gmail.com>
2020-05-04 19:20:16 +0800

    index_table change2

--- index.php
+++ index.php
@@ -4,32 +4,7 @@
  $validFunctions = array("hello","anotherF");
 
  $functName = $_REQUEST['f'];
- if(in_array($functName,$validFunctions))
- {
-     alert("wefwefwefwef");
-     $$functName();
- }else{
-     echo "You don't have permission to call that function so back off!";
-     exit();
- }
-
-
-
- function hello()
- {
-     echo "bye bye";
- }
-
- function anotherF()
- {
-     echo "the other funct";
- }
-
- function noTouch()
- {
-     echo "can't touch this!";
- }
-
+  echo $functName;
  ?>
 
  <!DOCTYPE html>
