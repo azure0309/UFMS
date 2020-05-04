e67aefc22455d9276b4c321755341e6eb3c28b4c
axe <axebnoo@gmail.com>
2020-05-04 20:45:59 +0800

    index_table change2

--- javascript/index_page/index_table.js
+++ javascript/index_page/index_table.js
@@ -137,8 +137,11 @@ $(document).on('click','.actionbutton', function(){
         call_to = targetRow[6].substring(1);
         console.log("CALL_TO: " + call_to);
 
+        clickedButton.text('Loading...');
+
         $.get("https://ufms.uni/Test/controller/index_page/block_action.php",{'user_num':call_to },function(data){
             alert("RESULT: " + data);
+            clickedButton.text('BLOCKED');
         });
 
         // clickedButton.removeClass("btn btn-info");
