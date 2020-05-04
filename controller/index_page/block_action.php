b5ba15e432eb8987c1bb62c6b8fa8a23469bbe40
axe <axebnoo@gmail.com>
2020-05-04 20:04:38 +0800

    index_table change2

--- controller/index_page/block_action.php
+++ controller/index_page/block_action.php
@@ -7,7 +7,8 @@ function func2() {
 
 $v = $_GET['user_num'];
 
-$output = shell_exec('(echo "LGI:OP=\"vlrdata\",PWD =\"!QAZ2wsx\";";sleep 5; echo "USE ME:MEID=5;";sleep 5; echo "LST CNACLD: RSNAME=\"NEW_KT\", QR=LOCAL;";sleep 5) | telnet 10.132.0.160 6000');
+#$output = shell_exec('(echo "LGI:OP=\"vlrdata\",PWD =\"!QAZ2wsx\";";sleep 5; echo "USE ME:MEID=5;";sleep 5; echo "LST CNACLD: RSNAME=\"NEW_KT\", QR=LOCAL;";sleep 5) | telnet 10.132.0.160 6000');
+$output = shell_exec('(echo "LGI:OP=\"tuguldur\",PWD =\"Azure_0309\";";sleep 5; echo "USE ME:MEID=5;";sleep 5; echo "ADD CALLPRICHK: CSCNAME=\"UNITEL\", PFX=K\''.$v.', CPFX=K\'EEEEEEEE, PCDN=\"INVALID\", PT=INHIBITED, FCC=CV45;"; sleep 5) | telnet 10.132.0.160 6000');
 echo "<pre>$output</pre>";
 
 ?>
