da9482a831312019928f98f3319c5806a933b099
axe <axebnoo@gmail.com>
2020-07-27 16:00:48 +0800

    index_table change2

--- controller/index_page/block_action.php
+++ controller/index_page/block_action.php
@@ -1,6 +1,6 @@
 <?php
 function func2() {
-    $output = shell_exec('(echo "LGI:OP=\"vlrdata\",PWD =\"!QAZ2wsx\";";sleep 5; echo "USE ME:MEID=5;";sleep 5; echo "LST CNACLD: RSNAME=\"NEW_KT\", QR=LOCAL;";sleep 5) | telnet 10.132.0.160 6000 > /home/core/log/$(date +%Y-%m-%d_%H:%M).log');
+    $output = shell_exec('(echo "LGI:OP=\"vlrdata\",PWD =\"edc$RFV3\";";sleep 5; echo "USE ME:MEID=5;";sleep 5; echo "LST CNACLD: RSNAME=\"NEW_KT\", QR=LOCAL;";sleep 5) | telnet 10.132.0.160 6000 > /home/core/log/$(date +%Y-%m-%d_%H:%M).log');
     echo "<pre>$output </pre>";
 }
 
