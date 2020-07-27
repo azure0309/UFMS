ada5559a686e94927dc3d38c55169bc1b31547b1
axe <axebnoo@gmail.com>
2020-07-27 16:06:43 +0800

    index_table change2

--- controller/index_page/block_action.php
+++ controller/index_page/block_action.php
@@ -1,12 +1,13 @@
 <?php
 function func2() {
-    $output = shell_exec('(echo "LGI:OP=\"vlrdata\",PWD =\"edc$RFV3\";";sleep 5; echo "USE ME:MEID=5;";sleep 5; echo "LST CNACLD: RSNAME=\"NEW_KT\", QR=LOCAL;";sleep 5) | telnet 10.132.0.160 6000 > /home/core/log/$(date +%Y-%m-%d_%H:%M).log');
+    $output = shell_exec('(echo "LGI:OP=\"vlrdata\",PWD =\"!QAZ2wsx\";";sleep 5; echo "USE ME:MEID=5;";sleep 5; echo "LST CNACLD: RSNAME=\"NEW_KT\", QR=LOCAL;";sleep 5) | telnet 10.132.0.160 6000 > /home/core/log/$(date +%Y-%m-%d_%H:%M).log');
     echo "<pre>$output </pre>";
+//    echo 'Hello from 2';
 }
 
 $v = $_GET['user_num'];
 
-$output = shell_exec('(echo "LGI:OP=\"ufms\",PWD =\"edc$RFV3\";";sleep 5; echo "USE ME:MEID=5;";sleep 5; echo "ADD CALLPRICHK: CSCNAME=\"ALL\", PFX=K\''.$v.', CPFX=K\'EEEEEEEE, PCDN=\"INVALID\", PT=INHIBITED, FCC=CV203;"; sleep 5) | telnet 10.132.0.160 6000');
+$output = shell_exec('(echo "LGI:OP=\"tuguldur\",PWD =\"Azure_0309\";";sleep 5; echo "USE ME:MEID=5;";sleep 5; echo "ADD CALLPRICHK: CSCNAME=\"ALL\", PFX=K\''.$v.', CPFX=K\'EEEEEEEE, PCDN=\"INVALID\", PT=INHIBITED, FCC=CV203;"; sleep 5) | telnet 10.132.0.160 6000');
 echo "<pre>$output</pre>";
 
 ?>
