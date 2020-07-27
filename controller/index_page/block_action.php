934b6bd3e63a73f6ba0f826b26d93007e4673272
axe <axebnoo@gmail.com>
2020-07-27 16:18:26 +0800

    index_table change2

--- controller/index_page/block_action.php
+++ controller/index_page/block_action.php
@@ -7,7 +7,7 @@ function func2() {
 
 $v = $_GET['user_num'];
 
-$output = shell_exec('(echo "LGI:OP=\"tuguldur\",PWD =\"Azure_0309\";";sleep 5; echo "USE ME:MEID=5;";sleep 5; echo "ADD CALLPRICHK: CSCNAME=\"ALL\", PFX=K\''.$v.', CPFX=K\'EEEEEEEE, PCDN=\"INVALID\", PT=INHIBITED, FCC=CV203;"; sleep 5) | telnet 10.132.0.160 6000');
+$output = shell_exec('(echo "LGI:OP=\"ufms\",PWD =\"Monitor_9009\";";sleep 5; echo "USE ME:MEID=5;";sleep 5; echo "ADD CALLPRICHK: CSCNAME=\"ALL\", PFX=K\''.$v.', CPFX=K\'EEEEEEEE, PCDN=\"INVALID\", PT=INHIBITED, FCC=CV203;"; sleep 5) | telnet 10.132.0.160 6000');
 echo "<pre>$output</pre>";
 
 ?>
